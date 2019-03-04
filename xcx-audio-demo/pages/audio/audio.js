9d3251bbea09a3e37ce3b97a95dbb3aa070524d2
xiexikang <1371419734@qq.com>
2019-03-04 14:25:28 +0800

    修复再次点击无法播放

--- xcx-audio-demo/pages/audio/audio.js
+++ xcx-audio-demo/pages/audio/audio.js
@@ -14,13 +14,13 @@ Page({
     audioArr: [
       {
         id: '000',
-        src: 'http://mp3.djwma.com/mp3/爆袭全站欢快节奏感觉那是杠杠滴.mp3',
+        src: 'https://s320.xiami.net/928/19928/1882512413/1772277226_1513175794617.mp3?ccode=xiami_web_web&expire=86400&duration=182&psid=485902a766977ecaae347a4cc851e8da&ups_client_netip=113.70.219.174&ups_ts=1551680441&ups_userid=0&utid=ljH9FKf0Um0CAQ7fs+HVAPRw&vid=1772277226&fn=1772277226_1513175794617.mp3&vkey=Bac0e9fde3983f090cef0a37e1cb73ef7',
         time: '30s',
         bl: false
       },
       {
         id: '001',
-        src: 'http://mp3.djwma.com/mp3/好听的欧美男声 网络流行.mp3',
+        src: 'https://s128.xiami.net/764/33764/2104642547/1810384080_1551417116476_5598.mp3?ccode=xiami_web_web&expire=86400&duration=181&psid=6320a01db73a6fdb9632f5800fc93fe5&ups_client_netip=113.70.219.174&ups_ts=1551680537&ups_userid=0&utid=ljH9FKf0Um0CAQ7fs+HVAPRw&vid=1810384080&fn=1810384080_1551417116476_5598.mp3&vkey=B54b264178201f6737c9bb5ff4bbce3d0',
         time: '50s',
         bl: false
       },
@@ -43,6 +43,8 @@ Page({
       audioArr[i].bl = false;
     }
     audioArr[key].bl = true;
+
+    myaudio.play();
 
     //开始监听
     myaudio.onPlay(() => {
